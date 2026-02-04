38d375933b9227b6c112694c0ba5cb0679755c0c
Pogorzelas <tomasz.pogorzelski97@gmail.com>
2026-02-04 12:09:34 +0100

    test: add data-testid to form submit button

--- src/components/product-list/product-card/edit-product-form/edit-product-form.component.tsx
+++ src/components/product-list/product-card/edit-product-form/edit-product-form.component.tsx
@@ -32,6 +32,7 @@ const EditProductForm = ({ defaultValues, onSubmit }: EditProductFormProps) => {
         <button
           type="submit"
           className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded transition-colors"
+          data-testid="product-form-submit"
         >
           Save
         </button>
